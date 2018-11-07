aedd166afa72f3f95b7b44c01c18554b41f04f07
Kilonzi <faithmueni6@gmail.com>
2018-11-07 01:16:20 +0000

    Catalog browsing changed and products page working

--- app.py
+++ app.py
@@ -34,13 +34,13 @@ mysql = MySQL(app)
 def index():
     session['index'] = True
     #This gets the featured products and pass them to the index page
-    featuredProducts1 = getProducts("SELECT * FROM products INNER JOIN categories ON products.product_id = categories.cat_id limit 3")
+    featuredProducts1 = getProducts("SELECT * FROM products INNER JOIN categories ON products.product_id = categories.cat_id ORDER BY RAND() limit 5")
     #This gets the featured products and pass them to the index page
-    featuredProducts2 = getProducts("SELECT * FROM products INNER JOIN categories ON products.product_id = categories.cat_id limit 3")
+    featuredProducts2 = getProducts("SELECT * FROM products INNER JOIN categories ON products.product_id = categories.cat_id ORDER BY RAND() limit 4")
     #This gets the latest products and pass them to the index page
-    latestProducts = getProducts("SELECT * FROM products INNER JOIN categories ON products.product_id = categories.cat_id limit 3")
+    latestProducts = getProducts("SELECT * FROM products INNER JOIN categories ON products.product_id = categories.cat_id ORDER BY RAND() limit 4")
     #This gets the picked products and pass them to the index page
-    pickedProducts = getProducts("SELECT * FROM products INNER JOIN categories ON products.product_id = categories.cat_id limit 3")
+    pickedProducts = getProducts("SELECT * FROM products INNER JOIN categories ON products.product_id = categories.cat_id ORDER BY RAND() limit 4")
     
     
     allProducts = getProducts("SELECT * FROM products INNER JOIN categories ON products.product_id = categories.cat_id")
@@ -58,7 +58,9 @@ def product():
 @app.route('/products')
 def products():
     session['index'] = False
-    return render_template("products.html")
+    allProducts = getProducts("SELECT * FROM products INNER JOIN categories ON products.product_id = categories.cat_id ORDER BY RAND()")
+    return render_template("products.html",ap = allProducts)
+
 
 
 #This is for rendering the checkout.html template
